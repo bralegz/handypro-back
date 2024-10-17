ebf4bf0c8012c11f6091beab4adb50bda01fb6fa
bralegz <bralegz@gmail.com>
2024-10-17 13:48:44 -0500

    typeorm config added // users entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import typeOrmConfig from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [typeOrmConfig] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/users/users.entity.ts
@@ -0,0 +1,64 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class Users {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  givenName: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  familyName: string;
+
+  //Needs to be nullable for google authenticaction
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  password: string;
+
+  //Needs to be nullable for google authenticaction
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  confirmationPassword: string;
+
+  //User will need a phone number
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  phone: string;
+
+  //User can edit profile picture
+  @Column({ type: 'varchar', nullable: true })
+  profileImg: string;
+
+  //User can be 'professional', 'client' or 'admin'
+  @Column({ type: 'varchar', nullable: true })
+  role: string;
+
+  //another entity? many to many?
+  @Column({ type: 'simple-array', nullable: true })
+  profession: string[];
+
+  //user will need location to use the services but won't define one when he registers.
+  @Column({ type: 'varchar', nullable: true })
+  location: string;
+
+  //calculated from all the reviews linked to this user
+  @Column({ type: 'float4', nullable: true })
+  rating: number;
+
+  //defined by the user
+  @Column({ type: 'simple-array', nullable: true })
+  services: string[];
+
+  //professional can switch his availability
+  @Column({ type: 'boolean', default: false })
+  availability: boolean;
+
+  //Photos can be added by the worker
+  @Column({ type: 'simple-array', nullable: true })
+  portfolio_gallery: string[];
+
+  //Years of experience can be added by the worker
+  @Column({ type: 'int2', nullable: true })
+  years_experience: number;
+}
