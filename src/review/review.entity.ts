72d10dd8f6b606c66a3f9aa521037d3971988d53
bralegz <bralegz@gmail.com>
2024-10-19 13:25:25 -0500

    review entity fields updated

--- src/review/review.entity.ts
+++ src/review/review.entity.ts
@@ -5,14 +5,9 @@ export class Review {
     @PrimaryGeneratedColumn('uuid')
     review_id: string;
 
-    @Column('uuid', { nullable: false })
-    professional_id: string; // FK
-
     @Column('decimal', { precision: 10, scale: 2, nullable: false })
     rating: number;
 
     @Column('varchar', { length: 50, nullable: false })
     comment: string;
-
-    // Faltan las relaciones
 }
