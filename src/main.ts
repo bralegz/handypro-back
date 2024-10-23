6ff09c21d2d1f9abef9f31d3573dc10c4b9bd27c
Carlos <139581721+Kuriarare@users.noreply.github.com>
2024-10-23 17:31:44 -0500

    Swagger basico para los amigos del front

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const swaggerConfig = new DocumentBuilder()
+    .setTitle('Proyecto Final para ver si nos graduamos')
+    .setDescription('HandyPro API description and documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
     app.enableCors({
         origin: '*',
     });
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
     constructor(private readonly usersService: UserService) {}
