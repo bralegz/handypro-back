89f3bf5b8c4efdb42b763afc2dc097f249517e82
bralegz <bralegz@gmail.com>
2024-10-25 19:42:09 -0500

    extract user info from google in google strategy and append it to request.user object

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -3,12 +3,14 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import googleOauthConfig from '../config/google-oauth.config';
 import { ConfigType } from '@nestjs/config';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
     constructor(
         @Inject(googleOauthConfig.KEY)
         private googleConfiguration: ConfigType<typeof googleOauthConfig>,
+        private authService: AuthService,
     ) {
         super({
             clientID: googleConfiguration.clientID,
@@ -28,5 +30,16 @@ export class GoogleStrategy extends PassportStrategy(Strategy) {
         //Otherwise we will just retreive the data from the database and return it in this function and it will be appended to the Req.user.
 
         console.log({ profile });
+        const user = await this.authService.validateGoogleUser({
+            email: profile.emails[0].value,
+            fullname: profile.displayName,
+            password: '',
+            confirmationPassword: '',
+            profileImg: profile.photos[0].value,
+        });
+
+        //first argument error object - second argument user data
+        //append user to request object
+        done(null, { ...user, role: '' });
     }
 }
