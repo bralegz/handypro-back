87c838511ab7b1e3c63160812f93ad4b84a316b9
bralegz <bralegz@gmail.com>
2024-10-27 14:47:11 -0500

    swagger docs for create application

--- src/application/application.controller.ts
+++ src/application/application.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { ApplicationService } from './application.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('application')
 @Controller('application')
@@ -19,7 +19,39 @@ export class ApplicationController {
     }
 
     //This route should be role protected and the professional id should be extracted from req.user
-    //The professional category should match the postedJob category
+    @ApiParam({
+        name: 'postedJobId',
+        type: 'string',
+        description: 'UUID del trabajo posteado',
+    })
+    @ApiParam({
+        name: 'professionalId',
+        type: 'string',
+        description: 'UUID del profesional que va a aplicar',
+    })
+    @ApiResponse({
+        status: 201,
+        description: 'La aplicación fue creada exitosamente',
+        schema: {
+            example: {
+                professional: {
+                    id: 'f2562866-c740-461f-ba9b-025a870c5d47',
+                    fullname: 'Javier Mendoza',
+                },
+                postedJob: {
+                    id: 'd388b2d2-a045-492a-9c04-58671f5c18df',
+                    title: 'La bañera se rompió',
+                },
+                id: '9ae693a7-26e2-4597-8d89-60b49b3d3052',
+                status: 'pendiente',
+            },
+        },
+    })
+    @ApiResponse({
+        status: 400,
+        description:
+            'El usuario debe existir, el trabajo posteado debe existir, debe estar pendiente, debe ser un trabajo al que el profesional no haya postulado antes, la categoria del trabajo y la categoria del profesional deben coincidir',
+    })
     @Post('apply/:postedJobId/:professionalId')
     async createApplication(
         @Param('postedJobId', ParseUUIDPipe) postedJobId: string,
