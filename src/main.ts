d1d0e7fa885e756466c3b4667a3fd918e5df34ec
bralegz <bralegz@gmail.com>
2024-10-28 11:30:19 -0500

    cors enabled only for localhost:3000 and credentials set to true

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,20 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     const swaggerConfig = new DocumentBuilder()
-    .setTitle('Proyecto Final para ver si nos graduamos')
-    .setDescription('HandyPro API description and documentation')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+        .setTitle('Proyecto Final para ver si nos graduamos')
+        .setDescription('HandyPro API description and documentation')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
 
     app.enableCors({
-        origin: '*',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  
-        allowedHeaders: 'Content-Type, Authorization',  
+        origin: 'http://localhost:3000',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Authorization',
+        credentials: true,
     });
 
     app.useGlobalPipes(new ValidationPipe());
