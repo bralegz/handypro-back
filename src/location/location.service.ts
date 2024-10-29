0745148bed4f0f321d77b324cf0924acd42761a3
bralegz <bralegz@gmail.com>
2024-10-29 10:22:01 -0500

    get all locations endpoint

--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { LocationRepository } from './location.repository';
 
 @Injectable()
@@ -6,5 +6,18 @@ export class LocationService {
     constructor(private readonly locationRepository: LocationRepository) {}
     getAllCategories() {
         return this.locationRepository.findAll();
+    }
+
+    getAllLocations() {
+        try {
+            const locations = this.locationRepository.getAllLocations();
+            if(!locations) {
+                throw new Error('No hay ubicaciones disponibles')
+            }
+
+            return locations
+        } catch (error) {
+            throw new NotFoundException(error.message);
+        }
     }
 }
