86624227715b4e530f0cf8d5cf81ce4a2dc19831
Carlos <139581721+Kuriarare@users.noreply.github.com>
2024-10-30 19:48:50 -0500

    pasarela de pagos con Stripe funcional

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {rawBody: true});
 
     const swaggerConfig = new DocumentBuilder()
         .setTitle('Proyecto Final para ver si nos graduamos')
@@ -17,14 +18,13 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
 
     app.enableCors({
-        origin: '*',
-        // ['http://localhost:3000', 'http://localhost:5173']
+        origin: ['http://localhost:3000', 'http://localhost:5173'],    
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         allowedHeaders: 'Content-Type, Authorization',
         credentials: true,
     });
-
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(3005);
+    
 }
 bootstrap();
